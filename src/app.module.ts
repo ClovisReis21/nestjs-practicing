4cd1bb1e2d0e81b63e9fa02040166b452bed41d3
Clovis <clovis.fmu@live.com>
2022-04-12 15:35:22 -0300

    First one

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { join } from 'path';import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { StudentsModule } from './modules/students/students.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot(),
+    StudentsModule,
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
